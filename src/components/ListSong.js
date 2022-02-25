b200c234e3ce90fc9daff95fb4e496883ee26a62
hiep1997xz <73546080+hiep1997xz@users.noreply.github.com>
2022-02-25 18:10:40 +0700

    feature/dataSong

--- src/components/ListSong.js
+++ src/components/ListSong.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
 
 const ListSong = () => {
   return (
-    <div>ListSong</div>
-  )
-}
+    <div className="col-span-2  overflow-y-scroll">
+      <table className="table-auto w-full">
+        <thead className="text-white h-12">
+          <tr>
+            <th className="w-[10%]">#</th>
+            <th className="text-left">Title</th>
+            <th className="w-[10%]">Author</th>
+            <th className="w-[10%]">
+              <i className="fa fa-download"></i>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="text-center">1</td>
+            <td>2</td>
+            <td className="text-center">3</td>
+            <td className="text-center">4</td>
+            <td className="text-center">5</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-export default ListSong
+export default ListSong;
