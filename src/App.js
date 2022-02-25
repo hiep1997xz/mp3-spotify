b08888cda1146e89f25db54d6cb1e7d5f9cfa52d
hiep1997xz <73546080+hiep1997xz@users.noreply.github.com>
2022-02-25 17:57:08 +0700

    feature/detail

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import DetailSong from "./components/DetailSong";
+import ListSong from "./components/ListSong";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <div className="grid grid-cols-3 bg-slate-700 h-screen-navbar-player overflow-hidden">
+        <DetailSong />
+        <ListSong />
+      </div>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className="h-24 bg-slate-900 text-white text-center leading-[6rem] text-3xl">
       <i className="fa fa-spotify mr-5"></i>
-      Spotify
+      Mp3 Spotify
     </div>
   );
 };
